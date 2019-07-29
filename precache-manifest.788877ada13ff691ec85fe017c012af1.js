d5e2101180729fe85d2272c750a575bbb192df36
Adm1ralDen <games.zadrotina@gmail.com>
2019-07-30 02:37:26 +0300

    Updates

---
+++ precache-manifest.788877ada13ff691ec85fe017c012af1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "415ce5dadb0e0f480f6a328e48879bb5",
+    "url": "/checkers-redux/index.html"
+  },
+  {
+    "revision": "613253c39ee4b438f352",
+    "url": "/checkers-redux/static/css/2.46439c26.chunk.css"
+  },
+  {
+    "revision": "0545a4c91b9e4cdc19b2",
+    "url": "/checkers-redux/static/css/main.f864ac22.chunk.css"
+  },
+  {
+    "revision": "613253c39ee4b438f352",
+    "url": "/checkers-redux/static/js/2.33b11cd7.chunk.js"
+  },
+  {
+    "revision": "0545a4c91b9e4cdc19b2",
+    "url": "/checkers-redux/static/js/main.9f40e637.chunk.js"
+  },
+  {
+    "revision": "3fb3a2fcd6f5160c8b4f",
+    "url": "/checkers-redux/static/js/runtime~main.035c0e00.js"
+  },
+  {
+    "revision": "4a182938e20d4558d292be21b107d0a5",
+    "url": "/checkers-redux/static/media/black.4a182938.png"
+  },
+  {
+    "revision": "2a63fe561b2e0023c7deb5fe8dafd64e",
+    "url": "/checkers-redux/static/media/blackQeen.2a63fe56.png"
+  },
+  {
+    "revision": "819a85fb32543eab3f9da5ac1a09e38d",
+    "url": "/checkers-redux/static/media/white.819a85fb.png"
+  },
+  {
+    "revision": "5c0d39b66fa3dbb8af903d62fcd8c265",
+    "url": "/checkers-redux/static/media/whiteQeen.5c0d39b6.png"
+  }
+]);
